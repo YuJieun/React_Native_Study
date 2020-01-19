f0034b924d285c193e8312d92a780021c9615407
YuJieun <idpresent@naver.com>
2020-01-19 15:15:05 +0900

    Merge

--- react_native_weather/fokin-weather/App.js
+++ react_native_weather/fokin-weather/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
+import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
-import { Alert } from "react-native";
 import axios from "axios";
-import Weather from "./Weather"
+import Weather from "./Weather";
 
-const API_KEY = "..";
+const API_KEY = "";
 
 export default class extends React.Component {
   state = {
